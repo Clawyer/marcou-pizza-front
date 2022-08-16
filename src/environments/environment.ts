55aaa1d241c652a806c16c1b77800fdb7ccb9ab7
Clawyer <valentin.deal@gmail.com>
2022-08-16 09:36:41 +0200

    remove environnement from github

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY = 'AIzaSyDXz7aN-GMIy4uNfm3qxXYkQAF0aDQ0g_I',
+  AUTH_DOMAIN = 'marcou-pizza.firebaseapp.com',
+  PROJECT_ID = 'marcou-pizza',
+  STORAGE_BUCKET = 'marcou-pizza.appspot.com',
+  MESSAGING_SENDER_ID = '621853670695',
+  APP_ID = '1:621853670695:web:439afe099be6b27ab57d02',
 };
 
 /*
@@ -13,4 +19,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error',  // Included with Angular CLI.
