b7e2fc747fe8e54f7e16d996b8b1ce5996f6a710
Clawyer <valentin.deal@gmail.com>
2022-08-18 16:28:08 +0200

    header complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, HeaderComponent, ProfileComponent],
+  imports: [BrowserModule, AppRoutingModule, CommonModule],
+providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  title: string = "Marcou Pizza"
-  constructor() { }
+  title: string = 'MARCOU PIZZAS';
+  subtitle: string = '05 63 62 85 86';
+  filterButtons = [
+    { text: 'home', icon: 'home.svg', isClicked: true },
+    { text: 'search', icon: 'search.svg', isClicked: false},
+    { text: 'account', icon: 'user.svg', isClicked: false },
+    { text: 'cart', icon: 'cart.svg', isClicked: false },
+  ];
+  isClicked: boolean = false;
 
-  ngOnInit(): void {
+  setActive(button: any): void {
+    for (let but of this.filterButtons) {
+      but.isClicked = false;
+      this.isClicked = false
+    }
+    if (button.text === 'search') this.isClicked = !this.isClicked;
+    button.isClicked = true;
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
